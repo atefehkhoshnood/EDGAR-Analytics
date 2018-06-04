80e71f473557838fa5c56a367a94c871162df5cf
Atefeh <atefeh.khoshnood@gmail.com>
2018-06-04 14:04:00 -0700

    Second commit: v1- working but not for repeated ips

--- src/sessionization.py
+++ src/sessionization.py
@@ -3,6 +3,19 @@ import re
 import csv
 from datetime import datetime, date, time, timedelta
 
+def is_ip_repeated(ip,data_dict):
+	i = 0
+	for key in data_dict:
+		if ip in key:
+			i +=1
+	if i == 1:
+		return (False,i)
+	else:
+		return (True,i)
+
+
+outputfile = open(os.curdir+'/output/sessionization.txt','w')
+
 file = open(os.curdir+'/input/inactivity_period.txt','r')
 inactivity_period =  int(file.read())
 file.close()
@@ -33,32 +46,107 @@ for row in reader:
 	#print('now, time is = ',time_now)
 
 	if counter_logfileline == 1:
-		time_start = time_now
+		time_past = time_now
 
-	time_diff = time_now - time_start
-	#isinstance(time_diff, timedelta)
-	time_diff_sec = time_diff.total_seconds()
-	if time_diff_sec > inactivity_period:
-		print('session is detected!')
-	 
+	if time_past != time_now:
+		data_maybe_inactive = []
+		data_maybe_inactive_repeated = {}
+		data_inactive = []
+
+
+		for key in data:
+			time_item = datetime.strptime(key[1][1],'%H:%M:%S')
+			time_diff = time_now - time_item
+			time_diff_sec = time_diff.total_seconds()
+			if time_diff_sec > inactivity_period:
+				data_maybe_inactive.append(key)
+				print('time difference is=',time_diff_sec)
+
+		for key in data_maybe_inactive:
+			ip_item = key[0]
+			i = 0
+			for k in data:
+				if ip_item in k:
+					i += 1
+			if i == 1:
+				outputfile.write(key[0]+','+key[1][0]+' '+key[1][1]+','+key[1][0]+' '+key[1][1]+',1,'+str(data[key])+'\n')
+				data_inactive.append(key)
+				print('session is detected!')
+			else:
+				data_maybe_inactive_repeated[key]=i
+
+		for key in data_maybe_inactive_repeated:
+			ip_item = key[0]
+			j = 0
+			for k in data_maybe_inactive_repeated:
+				if ip_item in k:
+					j += 1
+			if j == data_maybe_inactive_repeated[key]:
+				count_webpage_request = 0
+				i = 0
+				for k in data:
+					if k[0] == ip_item:
+						i +=1
+						count_webpage_request += data[k]
+						if i == 1:
+							time_start_0 = k[1][0]
+							time_start_1 = k[1][1]
+							time_start = datetime.strptime(k[1][1],'%H:%M:%S')
+						if i == j:
+							time_end_0 = k[1][0]
+							time_end_1 = k[1][1]
+							time_end = datetime.strptime(k[1][1],'%H:%M:%S')
+				time_diff = time_end - time_start
+				time_diff_sec = time_diff.total_seconds() + 1
+				outputfile.write(ip_item+','+time_start_0+' '+time_start_1+','+time_end_0+' '+time_end_1+','+str(int(time_diff_sec))+','+str(count_webpage_request)+'\n')
+				data_inactive.append(key)
+				print('session is detected!')
+
+
+		for key in data_inactive:
+			data.pop(key,0)
+
 	key = (row[ip_index],(row[date_index],row[time_index]))
 	if key in data:
 		data[key] += 1 
 	else:
 		data[key] = 1
 
+
+data_repeated = {}
+for key in data:
+	ip_item = key[0]
+	repeated, j = is_ip_repeated(ip_item,data)
+	if repeated:
+		data_repeated[ip_item] = j
+	else:
+		outputfile.write(key[0]+','+key[1][0]+' '+key[1][1]+','+key[1][0]+' '+key[1][1]+',1,'+str(data[key])+'\n')
+
+for ip_item in data_repeated:
+		count_webpage_request = 0
+		i = 0
+		for k in data:
+			if k[0] == ip_item:
+				i +=1
+				count_webpage_request += data[k]
+				if i == 1:
+					time_start_0 = k[1][0]
+					time_start_1 = k[1][1]
+					time_start = datetime.strptime(k[1][1],'%H:%M:%S')
+				if i == data_repeated[ip_item]:
+					time_end_0 = k[1][0]
+					time_end_1 = k[1][1]
+					time_end = datetime.strptime(k[1][1],'%H:%M:%S')
+		time_diff = time_end - time_start
+		time_diff_sec = time_diff.total_seconds() + 1
+		outputfile.write(ip_item+','+time_start_0+' '+time_start_1+','+time_end_0+' '+time_end_1+','+str(int(time_diff_sec))+','+str(count_webpage_request)+'\n')
+
 # 	removing item from dic: del faster than pop but pop has a second argument for avoiding exception errot
 #	del data[(row[ip_index],(row[date_index],row[time_index]))] 
 #	data.pop((row[ip_index],(row[date_index],row[time_index])),0)
 
-#	print('ip=',row[ip_index])
-#	print('date=',row[date_index])
-#	print('time=',row[time_index])
-#	print('cik=',row[cik_index])
-#	print('accession=',row[accession_index])
-#	print('extention=',row[extention_index])
-#	print('')
 
 #print('number of lines in log file = ',counter_logfileline)
 #print(headers)
+outputfile.close()
 print(data)
