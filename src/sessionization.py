6dfc18151f9d31d9748d204ebe2934961c734e57
Atefeh <atefeh.khoshnood@gmail.com>
2018-06-04 23:26:40 -0700

    Fourth commit: v3-all working

--- src/sessionization.py
+++ src/sessionization.py
@@ -3,7 +3,7 @@ import re
 import csv
 from datetime import datetime, date, time, timedelta
 
-def is_ip_repeated(ip,data_dict):
+def is_ip_repeated_dic(ip,data_dict):
 	i = 0
 	count_webpage_request = 0
 
@@ -15,6 +15,13 @@ def is_ip_repeated(ip,data_dict):
 		return (i,count_webpage_request)
 	else:
 		return (i,count_webpage_request)
+
+def is_ip_repeated_list(ip,data_list):
+	i = 0
+	for key in data_list:
+		if ip in key:
+			i +=1
+	return i
 
 
 outputfile = open(os.curdir+'/output/sessionization.txt','w')
@@ -53,7 +60,7 @@ for row in reader:
 
 	if time_past != time_now:
 		data_maybe_inactive = []
-		data_maybe_inactive_repeated = {}
+		data_written = []
 		data_inactive = []
 
 
@@ -68,43 +75,34 @@ for row in reader:
 		for key in data_maybe_inactive:
 			ip_item = key[0]
 			i = 0
-			for k in data:
-				if ip_item in k:
-					i += 1
-			if i == 1:
-				outputfile.write(key[0]+','+key[1][0]+' '+key[1][1]+','+key[1][0]+' '+key[1][1]+',1,'+str(data[key])+'\n')
+			no_repeated, count_webpage_request = is_ip_repeated_dic(ip_item,data)
+			no_repeated_list = is_ip_repeated_list(ip_item,data_maybe_inactive)
+
+			if no_repeated == 1:
+				outputfile.write(key[0]+','+key[1][0]+' '+key[1][1]+','+key[1][0]+' '+key[1][1]+',1,'+str(count_webpage_request)+'\n')
 				data_inactive.append(key)
 				print('session is detected!')
-			else:
-				data_maybe_inactive_repeated[key]=i
-
-		for key in data_maybe_inactive_repeated:
-			ip_item = key[0]
-			j = 0
-			for k in data_maybe_inactive_repeated:
-				if ip_item in k:
-					j += 1
-			if j == data_maybe_inactive_repeated[key]:
-				count_webpage_request = 0
+			elif (no_repeated != 1) and (no_repeated_list == no_repeated) and (ip_item not in data_written):
 				i = 0
 				for k in data:
 					if k[0] == ip_item:
 						i +=1
-						count_webpage_request += data[k]
 						if i == 1:
 							time_start_0 = k[1][0]
 							time_start_1 = k[1][1]
 							time_start = datetime.strptime(k[1][1],'%H:%M:%S')
-						if i == j:
+							print ('hi, t0')
+						if i == no_repeated_list:
 							time_end_0 = k[1][0]
 							time_end_1 = k[1][1]
 							time_end = datetime.strptime(k[1][1],'%H:%M:%S')
+							print ('hi, tf')
 				time_diff = time_end - time_start
 				time_diff_sec = time_diff.total_seconds() + 1
 				outputfile.write(ip_item+','+time_start_0+' '+time_start_1+','+time_end_0+' '+time_end_1+','+str(int(time_diff_sec))+','+str(count_webpage_request)+'\n')
+				data_written.append(ip_item)
 				data_inactive.append(key)
 				print('session is detected!')
-
 
 		for key in data_inactive:
 			data.pop(key,0)
@@ -120,7 +118,7 @@ data_repeated = {}
 for key in data:
 	ip_item = key[0]
 	print(ip_item)
-	no_repeated, count_webpage_request = is_ip_repeated(ip_item,data)
+	no_repeated, count_webpage_request = is_ip_repeated_dic(ip_item,data)
 	if no_repeated != 1 and ip_item not in data_repeated:
 			data_repeated[ip_item] = (key[1][1],no_repeated, count_webpage_request)
 print('this is data repeated')
