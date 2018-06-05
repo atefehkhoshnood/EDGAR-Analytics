484a1b64298531fe642ec1e22d727dacbc35ff76
Atefeh <atefeh.khoshnood@gmail.com>
2018-06-04 19:18:47 -0700

    Third commit: v2- working but not for repeated ips in the inactive search

--- src/sessionization.py
+++ src/sessionization.py
@@ -5,13 +5,16 @@ from datetime import datetime, date, time, timedelta
 
 def is_ip_repeated(ip,data_dict):
 	i = 0
+	count_webpage_request = 0
+
 	for key in data_dict:
 		if ip in key:
+			count_webpage_request += data_dict[key]
 			i +=1
 	if i == 1:
-		return (False,i)
+		return (i,count_webpage_request)
 	else:
-		return (True,i)
+		return (i,count_webpage_request)
 
 
 outputfile = open(os.curdir+'/output/sessionization.txt','w')
@@ -116,31 +119,35 @@ for row in reader:
 data_repeated = {}
 for key in data:
 	ip_item = key[0]
-	repeated, j = is_ip_repeated(ip_item,data)
-	if repeated:
-		data_repeated[ip_item] = j
-	else:
-		outputfile.write(key[0]+','+key[1][0]+' '+key[1][1]+','+key[1][0]+' '+key[1][1]+',1,'+str(data[key])+'\n')
-
-for ip_item in data_repeated:
-		count_webpage_request = 0
+	print(ip_item)
+	no_repeated, count_webpage_request = is_ip_repeated(ip_item,data)
+	if no_repeated != 1 and ip_item not in data_repeated:
+			data_repeated[ip_item] = (key[1][1],no_repeated, count_webpage_request)
+print('this is data repeated')
+print(data_repeated)
+print('\n')
+for key in data:
+	ip_item = key[0]
+	if (ip_item in data_repeated) and (data_repeated[ip_item][0] == key[1][1]):
 		i = 0
+		date_item, no_repeated, count_webpage_request = data_repeated[ip_item] 
 		for k in data:
 			if k[0] == ip_item:
 				i +=1
-				count_webpage_request += data[k]
 				if i == 1:
 					time_start_0 = k[1][0]
 					time_start_1 = k[1][1]
 					time_start = datetime.strptime(k[1][1],'%H:%M:%S')
-				if i == data_repeated[ip_item]:
+				if i == no_repeated:
 					time_end_0 = k[1][0]
 					time_end_1 = k[1][1]
 					time_end = datetime.strptime(k[1][1],'%H:%M:%S')
 		time_diff = time_end - time_start
 		time_diff_sec = time_diff.total_seconds() + 1
-		outputfile.write(ip_item+','+time_start_0+' '+time_start_1+','+time_end_0+' '+time_end_1+','+str(int(time_diff_sec))+','+str(count_webpage_request)+'\n')
 
+		outputfile.write(ip_item+','+time_start_0+' '+time_start_1+','+time_end_0+' '+time_end_1+','+str(int(time_diff_sec))+','+str(count_webpage_request)+'\n')
+	elif (ip_item not in data_repeated):
+		outputfile.write(key[0]+','+key[1][0]+' '+key[1][1]+','+key[1][0]+' '+key[1][1]+',1,'+str(data[key])+'\n')
 # 	removing item from dic: del faster than pop but pop has a second argument for avoiding exception errot
 #	del data[(row[ip_index],(row[date_index],row[time_index]))] 
 #	data.pop((row[ip_index],(row[date_index],row[time_index])),0)
